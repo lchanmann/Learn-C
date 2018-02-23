afb58467baef60af784605fa07dd486e377dda3e
Chanmann Lim <chanmannlim@gmail.com>
2018-02-23 15:43:32 -0500

    mergesort: return inversion count
    Add up inversion pairs count in merge function, left and right sub-arrays.

--- mergesort.c
+++ mergesort.c
@@ -8,54 +8,75 @@
 #define MAX_ARRAY_SIZE 20
 
 /* Helper functions declaration */
-void run_mergesort(int array[], int size);
+void runMergesort(int array[], int size);
 
 
 int main(void)
 {
     int array_0[] = { };
-    run_mergesort(array_0, 0);
+    runMergesort(array_0, 0);
 
     int array_1[] = { 1 };
-    run_mergesort(array_1, 1);
+    runMergesort(array_1, 1);
 
     int array_2[] = { 2, 1 };
-    run_mergesort(array_2, 2);
+    runMergesort(array_2, 2);
 
     int array_2_sorted[] = { 1, 2 };
-    run_mergesort(array_2_sorted, 2);
+    runMergesort(array_2_sorted, 2);
 
     int array_2_same[] = { 1, 1 };
-    run_mergesort(array_2_same, 2);
+    runMergesort(array_2_same, 2);
 
     int array_3_first_smallest[] = { 1, 3, 2 };
-    run_mergesort(array_3_first_smallest, 3);
+    runMergesort(array_3_first_smallest, 3);
 
     int array_3_last_largest[] = { 2, 1, 3 };
-    run_mergesort(array_3_last_largest, 3);
+    runMergesort(array_3_last_largest, 3);
 
     int array_3_middle_median[] = { 3, 2, 1 };
-    run_mergesort(array_3_middle_median, 3);
+    runMergesort(array_3_middle_median, 3);
 
     int array_even[4];
     populateArray(array_even, 4);
-    run_mergesort(array_even, 4);
+    runMergesort(array_even, 4);
 
     int array_odd[4];
     populateArray(array_odd, 5);
-    run_mergesort(array_odd, 5);
+    runMergesort(array_odd, 5);
 
     int array[MAX_ARRAY_SIZE];
     populateArray(array, MAX_ARRAY_SIZE);
-    run_mergesort(array, MAX_ARRAY_SIZE);
+    runMergesort(array, MAX_ARRAY_SIZE);
+
+    // Return inversion count from mergesort
+    //
+    // Inversion count is the count of out-of-order element pairs
+    // i.e. array[i] > array[j] and i < j
+    int an_array[] = { 38, 27, 43, 9, 82, 10 };
+    int size = sizeof(an_array) / sizeof(int);
+
+    // Inversion pairs in the array are:
+    // (38, 27)
+    // (82, 10)
+    // (27, 9) (38, 9) (43, 9)
+    // (27, 10) (38, 10) (43, 10)
+    // the total inversion count = 8
+    printf("mergesort(");
+    printArray(an_array, size);
+    printf("):\n");
+
+    int inv_count = mergesort(an_array, 0, size - 1);
+
+    printf("-- inversion count = %d\n", inv_count);
 }
 
 
-void run_mergesort(int array[], int size)
+void runMergesort(int array[], int size)
 {
     printf("mergesort(");
     printArray(array, size);
-    printf(") ->");
+    printf("):\n");
 
     mergesort(array, 0, size - 1);
 
@@ -63,21 +84,24 @@ void run_mergesort(int array[], int size)
     printf("\n");
 }
 
-void mergesort(int array[], int left, int right)
+int mergesort(int array[], int left, int right)
 {
+    int inv_count = 0;
     if (left < right) {
         int middle = (left + right) / 2;
 
         // Recursive mergesort on left and right sub-array
-        mergesort(array, left, middle);
-        mergesort(array, middle + 1, right);
+        inv_count = mergesort(array, left, middle);
+        inv_count += mergesort(array, middle + 1, right);
 
         // Merge and sort the array
-        merge(array, left, middle, right);
+        inv_count += merge(array, left, middle, right);
     }
+
+    return inv_count;
 }
 
-void merge(int array[], int left, int middle, int right)
+int merge(int array[], int left, int middle, int right)
 {
     int size = right - left + 1;
     int sorted[size];
@@ -85,13 +109,38 @@ void merge(int array[], int left, int middle, int right)
     int i = left;                   // left sub-array pointer
     int j = middle + 1;             // right sub-array pointer
     int k = 0;
+    int inv_count = 0;
 
     // Merge the sub-arrays and sort their elements along the way
     while (i <= middle) {
-        sorted[k++] = (j <= right && array[j] < array[i]) ?
-            array[j++] : array[i++];
+        if (j <= right && array[j] < array[i]) {
+            sorted[k] = array[j++];
+            // Mergesort presumes left and right sub-arrays are already sorted.
+            //
+            // When an element from the right sub-array is smaller one in the left,
+            // it is guaranteed to be smaller than the rest of the elements
+            // in the left sub-array. Thus, inversion pairs can be constructed
+            // with all of those elements.
+            inv_count += middle - i + 1;
+
+#ifdef _DEBUG_
+            // Print out inversion pairs
+            printf("-- inversion pairs: ");
+            for (int c = i; c <= middle; ++c) {
+                printf("(%d, %d) ", array[c], sorted[k]);
+            }
+            printf("\n");
+#endif /* _SHOW_INVERSION_PAIRS */
+        }
+        else {
+            sorted[k] = array[i++];
+        }
+        ++k;
     }
 
     // Update the original array with the sorted array
     memcpy(array + left, sorted, k * sizeof(int));
+
+    // Return inversion count
+    return inv_count;
 }
--- mergesort.h
+++ mergesort.h
@@ -1,7 +1,7 @@
 #ifndef _MERGE_SORT_H_
 #define _MERGE_SORT_H_
 
-void mergesort(int array[], int left, int right);
-void merge(int array[], int left, int middle, int right);
+int mergesort(int array[], int left, int right);
+int merge(int array[], int left, int middle, int right);
 
 #endif /* _MERGE_SORT_H_ */
