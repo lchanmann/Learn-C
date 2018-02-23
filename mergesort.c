3e3418148217a0ed0ab53279c71cdd6b712036db
Chanmann Lim <chanmannlim@gmail.com>
2018-02-22 19:37:32 -0500

    mergesort: implement merge routine

--- mergesort.c
+++ mergesort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "array_helpers.h"
 #include "mergesort.h"
@@ -29,4 +30,19 @@ void mergesort(int array[], int left, int right)
 
 void merge(int array[], int left, int middle, int right)
 {
+    int size = right - left + 1;
+    int sorted[size];
+
+    int i = left;                   // left sub-array pointer
+    int j = middle + 1;             // right sub-array pointer
+    int k = 0;
+
+    // Merge the sub-arrays and sort their elements along the way
+    while (i <= middle) {
+        sorted[k++] = (j <= right && array[j] < array[i]) ?
+            array[j++] : array[i++];
+    }
+
+    // Update the original array with the sorted array
+    memcpy(array + left, sorted, k * sizeof(int));
 }
